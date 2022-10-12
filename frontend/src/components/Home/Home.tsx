24751f4aeffa248b27ac8d22f0eb0c21303a8bd5
Adelina Voynova <adelina.voynova@student.manchester.ac.uk>
2022-10-12 05:49:55 +0100

    Dive into styled components

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import * as s from './Home.theme';
+import * as a from '../toolbox/AppBar'
+
 
 export const Home = () => {
     return <>
         <p> Welcome to the homepage!</p>
         <s.HomeButton>I do nothing!</s.HomeButton>
+        <a.StyledAppBar>Hey</a.StyledAppBar>
     </>
 }
