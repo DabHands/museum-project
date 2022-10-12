41332dd109f7fcf9ba86ec4d22f5c5ff4f2c25e3
Adelina Voynova <adelina.voynova@student.manchester.ac.uk>
2022-10-12 23:02:39 +0100

    Start styling the navbar

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import * as s from './Home.theme';
 import * as a from '../toolbox/AppBar'
 
-
 export const Home = () => {
     return <>
         <p> Welcome to the homepage!</p>
         <s.HomeButton>I do nothing!</s.HomeButton>
-        <a.StyledAppBar>Hey</a.StyledAppBar>
+        <a.StyledAppBar />
     </>
 }
--- frontend/src/components/toolbox/AppBar.tsx
+++ frontend/src/components/toolbox/AppBar.tsx
@@ -1,14 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const AppBar = ({ className, children }: any) => {
   return (
-    <span className={className}>
-      {children}
-    </span>
+  <StyledDiv>
+    <StyledSection>
+      <button aria-label="Home">Home</button>
+    </StyledSection>
+    <StyledSection role="toolbar">
+      <button aria-label="About">About</button>
+    </StyledSection>
+  </StyledDiv>
   );
 };
+
+const StyledSection = styled.section`
+display: inline;
+`;
+
+const StyledDiv = styled.div`
+background-color: purple;
+`;
 
 export const StyledAppBar = styled(AppBar)`
 color: palevioletred;
