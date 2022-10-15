d8f14ee77f343d7db0d8e67d3fbcd2ebea744362
Lucy Jefford <lucy.jefford@student.manchester.ac.uk>
2022-10-15 13:38:16 +0100

    Grid view working
    Also added some test data for the images (for now)

---
+++ frontend/src/components/Explore/GridView/GridView.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Embroidery } from '../../../types/types'
+import { GridSingle } from './GridSingle/GridSingle';
+import * as s from './GridView.theme';
+
+
+interface Props {
+    filteredEmbroideries: Embroidery[]
+}
+
+export const GridView: React.FC<Props> = (
+    { filteredEmbroideries }
+) => {
+
+    return (
+        <s.GridBox>
+            {filteredEmbroideries.map(e =>
+                <GridSingle embroidery={e}/>
+            )}
+        </s.GridBox>
+        )
+}
