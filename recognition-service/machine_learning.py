eb30c19709a2dbf035ca8f459b0fc8f740890795
Lucy Jefford <lucy.jefford@student.manchester.ac.uk>
2022-11-18 21:42:55 +0000

    Minor updates

--- recognition-service/machine_learning.py
+++ recognition-service/machine_learning.py
@@ -1,59 +1,54 @@
 from add_to_dataset import add, load
 
-
 '''
 Sources:
 https://www.tensorflow.org/tutorials/images/classification
 '''
 
+import pathlib
+
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-import pathlib
 
-## Get the dataset from online
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file(
-    'flower_photos', origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir)
+# ## Get the dataset from online
+# dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+# data_dir = tf.keras.utils.get_file(
+#     'flower_photos', origin=dataset_url, untar=True)
+# data_dir = pathlib.Path(data_dir)
 
 batch_size = 32
-img_height = 180
-img_width = 180
-
+img_height = 4032
+img_width = 3024
+data_dir = 'images/data'
 
 # Get the training and testing data
-train_ds = tf.keras.utils.image_dataset_from_directory(
+train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
     data_dir,
     validation_split=0.2,
-    subset="training",
+    subset="both",
     seed=123,
     image_size=(img_height, img_width),
-    batch_size=batch_size)
+    #color_mode='rgba',
+    batch_size=batch_size
+    )
 
-
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
 
 # This is the classes that we've identified from the dataset
 class_names = train_ds.class_names
 print(class_names)
+
+input('waiting for you to continue.... [ENTER]')
 
 #Basically load all the images
 AUTOTUNE = tf.data.AUTOTUNE
 
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
+print('just done step 1')
 
 #How many classes we are training on
 num_classes = len(class_names)
@@ -69,6 +64,7 @@ data_augmentation = keras.Sequential(
         layers.RandomZoom(0.1),
     ]
 )
+print('just done step 2')
 
 #The model, you can swap and change any of these
 model = Sequential([
@@ -85,17 +81,18 @@ model = Sequential([
     layers.Dense(128, activation='relu'),
     layers.Dense(num_classes, name="outputs")
 ])
+print('stated the model')
 
 #Make the model
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(
                   from_logits=True),
               metrics=['accuracy'])
-
+print('made the model')
 #print(model.summary())
 
 ## Train the model for 15 times
-epochs = 15
+epochs = 3
 history = model.fit(
     train_ds,
     validation_data=val_ds,
@@ -128,11 +125,12 @@ plt.show()
 
 ## Check on a random new image
 
-sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
-sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
+# sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
+# sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
 
+test_img_path = 'images/test_ooc/test_3'
 img = tf.keras.utils.load_img(
-    sunflower_path, target_size=(img_height, img_width)
+    test_img_path, target_size=(img_height, img_width)
 )
 img_array = tf.keras.utils.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0)  # Create a batch
