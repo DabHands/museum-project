6fab5fad0befd3f4a566b06762afd176d5a3df85
Adelina Voynova <voynova@progress.com>
2022-11-23 12:22:38 +0000

    Create homepage

--- frontend/src/components/Home/Home.theme.ts
+++ frontend/src/components/Home/Home.theme.ts
@@ -1,17 +1,104 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const HomeButton = styled.button`
-    
-    background-color : ${props => props.theme.colours.MAIN};
-    color: ${props => props.theme.colours.SHADING};
-    width: fit-content;
-    padding: 1em;
+const PageLayout = styled.div`
+  max-width: 60rem;
+  margin: 6rem auto;
 
-    &:hover{
-      background-color: ${props => props.theme.colours.SECONDARY};
-    }
+  display: grid;
+  grid-template-columns: 1fr 15rem 1fr;
+  grid-template-rows: 20rem 20rem 20rem 20rem;
+
+  row-gap: 8rem;
+`;
+
+const ImagesContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  height: 100%;
+  border-radius: 0.3rem;
+`;
+
+const ModelTextContainer = styled.div`
+  grid-column: 2 / -1;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TextWrapper = styled.div``;
+
+const TextHeading = styled.h3`
+  font-size: 1.8rem;
+  font-weight: 600;
+  color: #343a40;
+  margin-bottom: 1.5rem;
+  letter-spacing: 1;
+`;
+
+const Text = styled.p`
+  font-size: 1.4rem;
+  line-height: 1.6;
+
+  color: #495057;
+`;
+
+const TextButton = styled.button`
+  border: none;
+  background: none;
+  background-color: #343a40;
+  color: #FBC794;
+  cursor: pointer;
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin-top: 1.4rem;
+  padding: 0.7rem 1.4rem;
+  border-radius: 0.3rem;
+
+  &:hover {
+    background-color: #495057;
+    color: #fad4ad;
+  }
+`;
+
+const ScanImageTextContainer = styled.div`
+  grid-column: 1 / 3;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ExploreTextContainer = styled.div`
+  grid-column: 2 / -1;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AboutTextContainer = styled.div`
+  grid-column: 1 / 3;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export {
-    HomeButton
+  PageLayout,
+  ImagesContainer,
+  Image,
+  ModelTextContainer,
+  TextWrapper,
+  TextHeading,
+  Text,
+  TextButton,
+  ScanImageTextContainer,
+  ExploreTextContainer,
+  AboutTextContainer
 };
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -2,8 +2,82 @@ import React from 'react';
 import * as s from './Home.theme';
 
 export const Home: React.FC = () => {
-    return <>
-        <p> Welcome to the homepage!</p>
-        <s.HomeButton>I do nothing!</s.HomeButton>
-    </>
+  return (
+    <s.PageLayout>
+      <s.ImagesContainer>
+        <s.Image
+          src="../homepage/needle.jpg"
+          alt="Needle"
+        />
+      </s.ImagesContainer>
+      <s.ModelTextContainer>
+        <s.TextWrapper>
+          <s.TextHeading>3D Model</s.TextHeading>
+          <s.Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
+            itaque minus quae iste hic modi corrupti aut recusandae harum, ipsam
+            quibusdam maiores ex non corporis quos.
+          </s.Text>
+          <s.TextButton>View 3D Model</s.TextButton>
+        </s.TextWrapper>
+      </s.ModelTextContainer>
+
+      <s.ScanImageTextContainer>
+        <s.TextWrapper>
+          <s.TextHeading>Scan Image</s.TextHeading>
+          <s.Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+            rerum nam pariatur dolores repellendus ad? Aliquam laborum, dolore
+            magnam accusamus unde dolores blanditiis sunt expedita corrupti,
+            repellat minus amet ad?
+          </s.Text>
+          <s.TextButton>Scan An Image</s.TextButton>
+        </s.TextWrapper>
+      </s.ScanImageTextContainer>
+      <s.ImagesContainer>
+        <s.Image
+          src="../homepage/embr.jpg"
+          alt="Embroidery"
+        />
+      </s.ImagesContainer>
+
+      <s.ImagesContainer>
+        <s.Image
+          src="../homepage/needle.jpg"
+          alt="Needle"
+        />
+      </s.ImagesContainer>
+      <s.ExploreTextContainer>
+        <s.TextWrapper>
+          <s.TextHeading>Explore</s.TextHeading>
+          <s.Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
+            itaque minus quae iste hic modi corrupti aut recusandae harum, ipsam
+            quibusdam maiores ex non corporis quos facilis consequatur eligendi.
+            Veritatis!
+          </s.Text>
+          <s.TextButton>Check Out All Embroideries</s.TextButton>
+        </s.TextWrapper>
+      </s.ExploreTextContainer>
+
+      <s.AboutTextContainer>
+        <s.TextWrapper>
+          <s.TextHeading>About</s.TextHeading>
+          <s.Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+            rerum nam pariatur dolores repellendus ad? Aliquam laborum, dolore
+            magnam accusamus unde dolores blanditiis sunt expedita corrupti,
+            repellat minus amet ad?
+          </s.Text>
+          <s.TextButton>See More About The Artist</s.TextButton>
+        </s.TextWrapper>
+      </s.AboutTextContainer>
+      <s.ImagesContainer>
+        <s.Image
+          src="../homepage/embr.jpg"
+          alt="Embroidery"
+        />
+      </s.ImagesContainer>
+    </s.PageLayout>
+  )
 }
