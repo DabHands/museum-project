45262aa4f1eeeaef95fbe50bc932f42b550e2f8b
Lucy Jefford <50413397+LucyJefford@users.noreply.github.com>
2022-12-31 14:20:12 +0000

    Update explore page (#68)
    * Update filter bar padding
    
    * Update grid view to a more responsive layout

--- frontend/src/components/Explore/GridView/GridView.theme.ts
+++ frontend/src/components/Explore/GridView/GridView.theme.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 const GridBox = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-gap: 1rem;
 `;
 
 
