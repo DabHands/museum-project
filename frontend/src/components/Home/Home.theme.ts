72be6b00f36d6d111db31f863c0afad91e2df189
Lucy Jefford <lucy.jefford@student.manchester.ac.uk>
2023-02-15 16:44:17 +0000

    Homepage Mobile View

--- frontend/src/components/Home/Home.theme.ts
+++ frontend/src/components/Home/Home.theme.ts
@@ -1,114 +1,33 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components'
 
 const PageWrapper = styled.div`
+  padding: 10px;
   `;
 
-// const PageLayout = styled.div`
-//   max-width: 60rem;
-//   margin: 6rem auto;
+const PageText = styled.p`
 
-//   display: grid;
-//   grid-template-columns: 1fr 15rem 1fr;
-//   grid-template-rows: 18rem 18rem 18rem 18rem;
+`;
 
-//   row-gap: 8rem;
-// `;
+const Image = styled.img<{ centre?: boolean, individual?: boolean }>`
+    max-width: 100%;
+    height: auto;
+    object-fit: scale-down;
+    margin-bottom: 10px;
+    ${props => props.centre && css`
+        display: block; 
+        margin-left: auto; 
+        margin-right: auto; 
+        width: 50%;
+    `};
 
-// const ImagesContainer = styled.div`
-//   display: flex;
-//   justify-content: flex-start;
-//   align-items: center;
-// `;
-
-// const Image = styled.img`
-//   height: 100%;
-//   border-radius: 0.3rem;
-// `;
-
-// const ModelTextContainer = styled.div`
-//   grid-column: 2 / -1;
-
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `;
-
-// const TextWrapper = styled.div``;
-
-// const TextHeading = styled.h3`
-//   margin: 0;
-//   padding: 0;
-
-//   font-size: ${props => props.theme.fonts.size.MEDIUM};
-//   font-weight: 600;
-//   color: ${props => props.theme.homepage_colours.HEADING};
-//   margin-bottom: 1.5rem;
-//   letter-spacing: 1;
-// `;
-
-// const Text = styled.p`
-//   margin: 0;
-//   padding: 0;
-
-//   font-size: ${props => props.theme.fonts.size.SMALL};
-//   line-height: 1.6;
-//   margin-bottom: 1.4rem;
-
-//   color: ${props => props.theme.homepage_colours.TEXT};
-// `;
-
-// const TextButton = styled.button`
-//   border: none;
-//   background: none;
-//   background-color: ${props => props.theme.homepage_colours.BUTTON};
-//   color: ${props => props.theme.homepage_colours.TEXT_BUTTON};
-//   cursor: pointer;
-//   font-size: ${props => props.theme.fonts.size.MEDIUM};
-//   font-weight: 500;
-//   padding: 0.7rem 1.4rem;
-//   border-radius: 0.3rem;
-
-//   &:hover {
-//     background-color: ${props => props.theme.homepage_colours.BUTTON_HOVER};
-//     color: ${props => props.theme.homepage_colours.TEXT_BUTTON_HOVER};
-//   }
-// `;
-
-// const ScanImageTextContainer = styled.div`
-//   grid-column: 1 / 3;
-
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `;
-
-// const ExploreTextContainer = styled.div`
-//   grid-column: 2 / -1;
-
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `;
-
-// const AboutTextContainer = styled.div`
-//   grid-column: 1 / 3;
-
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `;
+    ${props => props.individual && css`
+        width: 50vw;
+    `};
+    
+`;
 
 export {
   PageWrapper,
-  // PageLayout,
-  // ImagesContainer,
-  // Image,
-  // ModelTextContainer,
-  // TextWrapper,
-  // TextHeading,
-  // Text,
-  // TextButton,
-  // ScanImageTextContainer,
-  // ExploreTextContainer,
-  // AboutTextContainer
+  PageText,
+  Image,
 };
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import * as s from './Home.theme';
-import { MainH1 } from '../toolbox/Labels';
+import embr from '../../util/homepage/embr.jpg'
+
+import logo from '../../util/logo/wide_logo.jpg'
+import { MainH1, SecondaryH1 } from '../toolbox/Labels';
+import MediaQuery from 'react-responsive';
+import { CaptionedImage } from '../toolbox/CaptionedImage/CaptionedImage';
+import { Button } from '../toolbox/Buttons';
+import NavigationContext from '../../contexts/NavigationContext';
 
 export const Home: React.FC = () => {
-  // const navigationContext = useContext(NavigationContext);
+  const navigationContext = useContext(NavigationContext);
+
+  const renderMobileView = () => {
+    return <>
+      <s.PageText>
+        “Dab Hands: That&apos;ll Need Stitching” is an exhibition in the
+        Manchester Museum, from the 8th March to 31st July 2023. 
+      </s.PageText>
+      <s.Image src={logo} />
+      <s.PageText>
+        In the museum, youll see the sculpture of the hand, with 
+        various embroideries made by artists from across Manchester all over it.
+      </s.PageText>
+      <Button onClick={navigationContext.navigateToModelPage}>
+        See the 3D Model </Button>
+      <s.PageText> Online, you can view the 3D model, or explore each embroidery
+        and the information behind each anatomy. Learn about the Hand, and the Story
+        of how this whole project came to be.
+      </s.PageText>
+      <Button onClick={navigationContext.navigateToExplorePage}>
+        Explore the Embroideries </Button>
+      <CaptionedImage image={embr} caption="One of the many embroideries on show at the exhibit." />
+      <Button onClick={navigationContext.navigateToStoryPage}>
+        The Story of the Sculpture </Button>
+      <s.PageText>
+        This is but one of the pieces in the wider Dab Hands Collection, 
+        which was created in residence at the Manchester Museum by Lucy
+        Burscough and her collaborative partners.
+      </s.PageText>
+      <Button onClick={navigationContext.navigateToAboutPage}>
+        Read more about the Dab Hands Project.</Button>
+    </>
+  }
+
+  const renderDesktopView = () => {
+    return <p>Desktop</p>
+  }
 
   return (
     <s.PageWrapper>
-      <MainH1 toPad> Dab Hands: That&apos;ll Need Stitching</MainH1>
+      <MediaQuery minWidth={800}> {/*The LARGE ONE*/}
+        <MainH1> Dab Hands: That&apos;ll Need Stitching</MainH1>
+        {renderDesktopView()}
+      </MediaQuery>
+
+      <MediaQuery maxWidth={799}> {/*The SMALL ONE*/}
+        <MainH1 style={{ width: 'auto' }}>
+          Dab Hands: That&apos;ll Need Stitching
+        </MainH1>
+        {renderMobileView()}
+      </MediaQuery>
     </s.PageWrapper>
     
     // <s.PageLayout>
