4ceb2c7630afc57a77a2a93726f083005b2e8ac7
Adelina Voynova <adelina.voynova12@gmail.com>
2023-02-20 15:57:41 +0000

    Add the model to the page as an .obj file

--- frontend/src/components/Model/Model.tsx
+++ frontend/src/components/Model/Model.tsx
@@ -1,97 +1,127 @@
 import React, { useEffect } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import Stats from 'three/examples/jsm/libs/stats.module';
-
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { Raycaster } from 'three';
 import * as s from './Model.theme';
 
 export const Model: React.FC = () => {
-    useEffect(() => {
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 4;
+  // create a variable to store the object
+  let model: any;
+  let canvasPositionLeft: number;
+  let canvasPositionTop: number;
+  let canvasPositionWidth: number;
+  let canvasPositionHeight: number;
 
-        const canvas = document.getElementById('myThreeJsCanvas') as HTMLElement;
-        const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, });
-        const controls = new OrbitControls(camera, renderer.domElement);
+  // set up the needed variables for the scene
+  const rayCaster = new Raycaster();
+  // create the scene
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 4;
 
-        const keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-        keyLight.position.set(-100, 0, 100);
+  const setCanvasPosition = ( canvas: HTMLElement) => {
+    canvasPositionLeft = canvas.offsetLeft;
+    canvasPositionTop = canvas.offsetTop;
+    canvasPositionHeight = canvas.offsetHeight;
+    canvasPositionWidth = canvas.offsetWidth;
 
-        const fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-        fillLight.position.set(100, 0, 100);
+    return { canvasPositionLeft, canvasPositionTop, canvasPositionHeight, canvasPositionWidth};
+  };
 
-        const backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-        backLight.position.set(100, 0, -100).normalize();
+  const addModelLights = () => {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+    scene.add(ambientLight);
 
-        const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
-        scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
+    directionalLight.position.set(0, -2, -5);
+    scene.add(directionalLight);
+  };
 
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
-        directionalLight.position.set(0, -2, -5);
-        scene.add(directionalLight);
+  const loadModel = () => {
+    // load the materials fot the model, with the model and its texture
+    const mtlLoader = new MTLLoader();
+    mtlLoader.load("polycam-mesh/poly.mtl", function (materials) {
+      materials.preload();
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(materials);
+      objLoader.load(
+        'polycam-mesh/poly-meshlab.obj',
+        function (object) {
+          // load the texture
+          model = { ...object };
+          const textureLoader = new THREE.TextureLoader();
+          const texture = textureLoader.load('polycam-mesh/poly_0_ox0Gp9Nh.jpg');
 
-        // scene.add(keyLight);
-        // scene.add(fillLight);
-        // scene.add(backLight);
-
-        renderer.setSize(window.innerWidth, window.innerHeight);
-
-        const loader = new GLTFLoader();
-        // Load a glTF resource
-        loader.load(
-            // resource URL
-            'test.glb',
-            // called when the resource is loaded
-            function (glb) {
-
-                scene.add(glb.scene);
-
-                glb.animations; // Array<THREE.AnimationClip>
-                glb.scene; // THREE.Group
-                glb.scenes; // Array<THREE.Group>
-                glb.cameras; // Array<THREE.Camera>
-                glb.asset; // Object
-
-            },
-            // called while loading is progressing
-            function (xhr) {
-
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-            },
-            // called when loading has errors
-            function (error) {
-
-                console.log('An error happened', error);
-
+          // append the texture to the model
+          object.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+              child.material.map = texture;
             }
-        );
-
-
-        // const geometry = new THREE.BoxGeometry(1, 1, 1);
-        // const material = new THREE.MeshBasicMaterial({ color: 0xa16c38 });
-        // const cube = new THREE.Mesh(geometry, material);
-        // scene.add(cube);
-
-        const stats = Stats()
-        document.body.appendChild(stats.dom)
-
-        function animate() {
-            requestAnimationFrame(animate);
-            controls.update();
-            // cube.rotation.x += 0.01;
-            // cube.rotation.y += 0.01;
-
-            renderer.render(scene, camera);
+          });
+          scene.add(object);
+        },
+        function (xhr) {
+          console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function (error) {
+          console.log('An error happened', error);
         }
-        animate();
-    }, []);
+      );
+    });
+  };
 
-    return (<s.PageWrapper>
-        <div style={{ marginTop: "10px" }}>
-            <canvas id="myThreeJsCanvas" style={{ position: "absolute" }}></canvas>
-        </div>
-    </s.PageWrapper>)
+  useEffect(() => {
+    // position the depth of the camera
+    const canvas = document.getElementById('modelCanvas') as HTMLElement;
+
+    const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, });
+
+    // add controls to the able to move and zoom the model
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    // add lights to the model
+    addModelLights();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    loadModel();
+
+    // show the model
+    function animate() {
+      requestAnimationFrame(animate);
+      controls.update();
+      renderer.render(scene, camera);
+    }
+    animate();
+  });
+
+  const handleClickOnCanvas = (event: any) => {
+    event.preventDefault();
+
+    const canvas = document.getElementById('modelCanvas') as HTMLElement;
+    const {canvasPositionLeft, 
+      canvasPositionTop, 
+      canvasPositionHeight, 
+      canvasPositionWidth } = setCanvasPosition(canvas);
+
+    const x = ((event.clientX - canvasPositionLeft) / canvasPositionWidth) * 2 - 1;
+    const y = -((event.clientY - canvasPositionTop) / canvasPositionHeight) * 2 + 1;
+
+    const mousePosition = new THREE.Vector2(x, y);
+
+    rayCaster.setFromCamera(mousePosition, camera);
+    const intersects = rayCaster.intersectObjects(model.children, true);
+
+    if (intersects.length > 0) {
+      console.log(intersects[0].point);
+    }
+  };
+
+  return (<s.PageWrapper>
+    <div style={{ marginTop: "10px" }}>
+      <canvas id="modelCanvas" style={{ position: "absolute" }} onClick={handleClickOnCanvas}></canvas>
+    </div>
+  </s.PageWrapper>)
 }
