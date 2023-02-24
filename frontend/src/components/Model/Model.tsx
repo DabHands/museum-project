d3791cd084b87b0b2ae9d13066c0365bf75e5f2a
Adelina Voynova <adelina.voynova12@gmail.com>
2023-02-24 13:02:23 +0000

    Prepare for release 3.2

--- frontend/src/components/Model/Model.tsx
+++ frontend/src/components/Model/Model.tsx
@@ -1,34 +1,44 @@
 import React, { useEffect } from 'react';
+import MediaQuery from 'react-responsive';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 import { Raycaster } from 'three';
+import { AllEmbroideries } from '../../data/embroideries';
+import { SphereInfo } from '../../types/types';
 import * as s from './Model.theme';
 
 export const Model: React.FC = () => {
-  // create a variable to store the object
+
+  // const embroideryCentre  = [0.2930, -0.3407, 0.2198];
+  // const embroideryEnd = [0.2108, -0.3944, 0.1594];
+
+  // let sphereInfo: SphereInfo;
+
+  // // create a variable to store the object
   let model: any;
-  let canvasPositionLeft: number;
-  let canvasPositionTop: number;
-  let canvasPositionWidth: number;
-  let canvasPositionHeight: number;
+  // let canvasPositionLeft: number;
+  // let canvasPositionTop: number;
+  // let canvasPositionWidth: number;
+  // let canvasPositionHeight: number;
 
   // set up the needed variables for the scene
-  const rayCaster = new Raycaster();
+  // const rayCaster = new Raycaster();
   // create the scene
   const scene = new THREE.Scene();
+  // scene.background = new THREE.Color( 0xff0000 );
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 4;
 
-  const setCanvasPosition = (canvas: HTMLElement) => {
-    canvasPositionLeft = canvas.offsetLeft;
-    canvasPositionTop = canvas.offsetTop;
-    canvasPositionHeight = canvas.offsetHeight;
-    canvasPositionWidth = canvas.offsetWidth;
+  // const getCanvasPosition = (canvas: HTMLElement) => {
+  //   canvasPositionLeft = canvas.offsetLeft;
+  //   canvasPositionTop = canvas.offsetTop;
+  //   canvasPositionHeight = canvas.offsetHeight;
+  //   canvasPositionWidth = canvas.offsetWidth;
 
-    return { canvasPositionLeft, canvasPositionTop, canvasPositionHeight, canvasPositionWidth };
-  };
+  //   return { canvasPositionLeft, canvasPositionTop, canvasPositionHeight, canvasPositionWidth };
+  // };
 
   const addModelLights = () => {
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
@@ -47,12 +57,12 @@ export const Model: React.FC = () => {
       const objLoader = new OBJLoader();
       objLoader.setMaterials(materials);
       objLoader.load(
-        'polycam-mesh/poly-meshlab.obj',
+        'polycam/poly.obj',
         function (object) {
           // load the texture
           model = { ...object };
           const textureLoader = new THREE.TextureLoader();
-          const texture = textureLoader.load('polycam-mesh/poly_0_ox0Gp9Nh.jpg');
+          const texture = textureLoader.load('polycam/texture.jpg');
 
           // append the texture to the model
           object.traverse(function (child) {
@@ -72,31 +82,39 @@ export const Model: React.FC = () => {
     });
   };
 
-  const createPlane = (scene: THREE.Scene) => {
-    const plane = new THREE.BufferGeometry();
+  // const calculateSphereRadiusOnEmbroidery = (embroideryCentre: number[], embroideryEnd: number[]) => {
+  //   const x = (embroideryEnd[0] - embroideryCentre[0]) * (embroideryEnd[0] - embroideryCentre[0]);
+  //   const y = (embroideryEnd[1] - embroideryCentre[1]) * (embroideryEnd[1] - embroideryCentre[1]);
+  //   const z = (embroideryEnd[2] - embroideryCentre[2]) * (embroideryEnd[2] - embroideryCentre[2]);
 
-    const vertices = new Float32Array([
-      0.42673003149717437, -0.5054964491241991, 0.9481348893858136,
-      0.480864406523007, -0.5837215299495722, 0.8310803664288977,
-      0.41161666435286415, -0.5939965223753978, 0.7088020270774806,
-      0.2636958064938275, -0.5696199149816064, 0.7019622114722872,
-      0.1956546599158516, -0.5192302765873366, 0.825893673636176,
-      0.2783881104931201, -0.4926125296728612, 0.9501636605944817
-    ]);
+  //   const radius = Math.sqrt(x + y + z);
+  //   return radius;
+  // };
 
-    plane.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    const mesh = new THREE.Mesh(plane, material);
+  // const createSphere = (scene: THREE.Scene, embroideryCentre: number[], embroideryEnd: number[]) => {
+  //   const embroideryRadius = calculateSphereRadiusOnEmbroidery(embroideryCentre, embroideryEnd);
 
-    scene.add(mesh);
-  };
+  //   const geometry = new THREE.SphereGeometry( embroideryRadius, 32, 16 );
+  //   const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+  //   const sphere = new THREE.Mesh( geometry, material );
+  //   sphere.position.x = embroideryCentre[0];
+  //   sphere.position.y = embroideryCentre[1];
+  //   sphere.position.z = embroideryCentre[2];
 
+  //   sphere.material.transparent = true;
+  //   sphere.material.opacity = 0.0;
+
+  //   scene.add(sphere);
+  //   return sphere;
+  // };
+
+  // let sphere: THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial> | THREE.Object3D<THREE.Event>;
 
   useEffect(() => {
-    // position the depth of the camera
     const canvas = document.getElementById('modelCanvas') as HTMLElement;
 
-    const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, });
+    const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
+    renderer.setClearColor(0x000000, 0);
 
     // add controls to the able to move and zoom the model
     const controls = new OrbitControls(camera, renderer.domElement);
@@ -107,7 +125,9 @@ export const Model: React.FC = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     loadModel();
-    // createPlane(scene);
+    // sphere = createSphere(scene, embroideryCentre, embroideryEnd);
+    // sphereInfo = { embroideryId: "98ef96da-e1d3-49b0-8029-6bc405997dd7",
+    // sphere: sphere}
 
     // show the model
     function animate() {
@@ -125,7 +145,7 @@ export const Model: React.FC = () => {
   //   const { canvasPositionLeft,
   //     canvasPositionTop,
   //     canvasPositionHeight,
-  //     canvasPositionWidth } = setCanvasPosition(canvas);
+  //     canvasPositionWidth } = getCanvasPosition(canvas);
 
   //   const x = ((event.clientX - canvasPositionLeft) / canvasPositionWidth) * 2 - 1;
   //   const y = -((event.clientY - canvasPositionTop) / canvasPositionHeight) * 2 + 1;
@@ -135,14 +155,26 @@ export const Model: React.FC = () => {
   //   rayCaster.setFromCamera(mousePosition, camera);
   //   const intersects = rayCaster.intersectObjects(model.children, true);
 
+  //   const sphereIntersects = rayCaster.intersectObject(sphere);
+
   //   if (intersects.length > 0) {
   //     console.log(intersects[0].point);
+  //     // scene.add(new THREE.ArrowHelper(rayCaster.ray.direction, rayCaster.ray.origin, 300, 0xff0000) );
   //   }
+
+  //   if (sphereIntersects.length > 0) {
+  //     console.log('ayeeee');
+  //   }
+
+  //   // console.log('AllEmbroideries', AllEmbroideries.length);
+  //   // const embroidery = AllEmbroideries.find(embroidery => embroidery.id == "98ef96da-e1d3-49b0-8029-6bc405997dd7");
+  //   // console.log("embroidery.name", embroidery?.anatomyName);
   // };
 
-  return (<s.PageWrapper>
-    <div style={{ marginTop: "10px" }}>
-      <canvas id="modelCanvas"></canvas>
-    </div>
-  </s.PageWrapper>)
+  return (
+    <s.PageWrapper>
+      <s.ModelWrapper>
+        <s.ModelCanvas id="modelCanvas"></s.ModelCanvas>
+      </s.ModelWrapper>
+    </s.PageWrapper>)
 }
