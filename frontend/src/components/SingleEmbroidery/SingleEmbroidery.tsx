975b8b8e5877a06dc33ba69a50617eee7254280a
Lucy Jefford <50413397+LucyJefford@users.noreply.github.com>
2023-02-24 12:32:37 +0000

    Back Button (#92)
    * Back to Explore button
    
    * Can navigate between the pages

--- frontend/src/components/SingleEmbroidery/SingleEmbroidery.tsx
+++ frontend/src/components/SingleEmbroidery/SingleEmbroidery.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import MediaQuery from 'react-responsive';
 import { useParams } from 'react-router-dom';
 import { AllAnatomies } from '../../data/anatomies';
 import { AllEmbroideries } from '../../data/embroideries';
 import { SecondaryH1 } from '../toolbox/Labels';
+import {BsArrowLeftShort} from 'react-icons/bs'
 import * as s from './SingleEmbroidery.theme';
+import NavigationContext from '../../contexts/NavigationContext';
 
 export const SingleEmbroidery: React.FC = () => {
     const { id } = useParams();
-
+    const navigationContext = useContext(NavigationContext);
     //Now to get the emboidery itself
     const em = AllEmbroideries.find(e => e.id === id)
     //And get the anatomy that uses it
     const an = em ? AllAnatomies.find(a => a.name === em.anatomyName) : null;
-
+    window.scrollTo(0, 0)
+    
     const getName = () => {
         if (an) {
             if (an.longName) {
@@ -47,9 +50,18 @@ export const SingleEmbroidery: React.FC = () => {
         )
     }
 
+    const renderBackButton = () => {
+        return (
+            <s.IconButton onClick={navigationContext.navigateToExplorePage}> 
+                <BsArrowLeftShort/> Back to Explore
+            </s.IconButton>
+        )
+    }
+
     const renderMobileView = () => {
         return (
             <>
+                {renderBackButton()}
                 <SecondaryH1> {getName()}</SecondaryH1>
                 <p>
                     Embroidered By
@@ -65,7 +77,12 @@ export const SingleEmbroidery: React.FC = () => {
     const renderDesktopView = () => {
         return (
             <>
-                <SecondaryH1> {getName()}</SecondaryH1>
+                <s.TitleBox>
+                    <SecondaryH1> {getName()}</SecondaryH1>
+                    {renderBackButton()}
+                </s.TitleBox>
+                
+                
                 
                  <s.SplitPage>
                     
@@ -77,7 +94,9 @@ export const SingleEmbroidery: React.FC = () => {
                         {renderImage()}
                         
                     </div>
+                    
                     {renderDescription()}
+
                  </s.SplitPage>
             </>
         )
