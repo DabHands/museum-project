fdda7b1a15bb398ac927449463d128b4c6462ead
adyvoynova <107405811+adyvoynova@users.noreply.github.com>
2023-02-27 23:02:06 +0000

    Model/make model interactive (#94)
    * Upload final version of the model
    
    * Add the coordinates
    
    * Add a popup on click
    
    * Create context to fix undefined model issue
    
    * Resize canvas
    
    * Move thumbnail images to the public folder
    
    * store the camera position at all times
    
    * Do not show popup when undefined
    
    * Comment out redundant code
    
    * Disable lint

--- frontend/src/components/Model/Model.tsx
+++ frontend/src/components/Model/Model.tsx
@@ -1,44 +1,51 @@
-import React, { useEffect } from 'react';
+/* eslint-disable */
+import React, { useContext, useEffect, useState } from 'react';
 // import MediaQuery from 'react-responsive';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
-// import { Raycaster } from 'three';
+import { Raycaster } from 'three';
 // import { AllEmbroideries } from '../../data/embroideries';
 // import { SphereInfo } from '../../types/types';
 import * as s from './Model.theme';
+import ModelContext from '../../contexts/ModelContext';
+import { AllEmbroideries } from '../../data/embroideries';
+import { Embroidery, PopupEmbroideryInfo, SphereInfo } from '../../types/types';
+import { Popup } from './Popup';
 
 export const Model: React.FC = () => {
+  const modelContext = useContext(ModelContext);
 
-  // const embroideryCentre  = [0.2930, -0.3407, 0.2198];
-  // const embroideryEnd = [0.2108, -0.3944, 0.1594];
+  const [shouldShowPopup, setShouldShowPopup] = useState<boolean>(false);
+  const [popupEmbroideryInfo, setPopupEmbroideryInfo] = useState<PopupEmbroideryInfo>();
 
-  // let sphereInfo: SphereInfo;
+  let spheresLength: number;
+  const spheres: SphereInfo[] = [];
 
   // // create a variable to store the object
-  // let model: any;
-  // let canvasPositionLeft: number;
-  // let canvasPositionTop: number;
-  // let canvasPositionWidth: number;
-  // let canvasPositionHeight: number;
+  let model: any;
+  let canvasPositionLeft: number;
+  let canvasPositionTop: number;
+  let canvasPositionWidth: number;
+  let canvasPositionHeight: number;
 
   // set up the needed variables for the scene
-  // const rayCaster = new Raycaster();
+  const rayCaster = new THREE.Raycaster();
   // create the scene
   const scene = new THREE.Scene();
   // scene.background = new THREE.Color( 0xff0000 );
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 4;
 
-  // const getCanvasPosition = (canvas: HTMLElement) => {
-  //   canvasPositionLeft = canvas.offsetLeft;
-  //   canvasPositionTop = canvas.offsetTop;
-  //   canvasPositionHeight = canvas.offsetHeight;
-  //   canvasPositionWidth = canvas.offsetWidth;
+  const getCanvasPosition = (canvas: HTMLElement) => {
+    canvasPositionLeft = canvas.offsetLeft;
+    canvasPositionTop = canvas.offsetTop;
+    canvasPositionHeight = canvas.offsetHeight;
+    canvasPositionWidth = canvas.offsetWidth;
 
-  //   return { canvasPositionLeft, canvasPositionTop, canvasPositionHeight, canvasPositionWidth };
-  // };
+    return { canvasPositionLeft, canvasPositionTop, canvasPositionHeight, canvasPositionWidth };
+  };
 
   const addModelLights = () => {
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
@@ -52,7 +59,7 @@ export const Model: React.FC = () => {
   const loadModel = () => {
     // load the materials fot the model, with the model and its texture
     const mtlLoader = new MTLLoader();
-    mtlLoader.load("polycam-mesh/poly.mtl", function (materials) {
+    mtlLoader.load("polycam/final-project.mtl", function (materials) {
       materials.preload();
       const objLoader = new OBJLoader();
       objLoader.setMaterials(materials);
@@ -60,9 +67,10 @@ export const Model: React.FC = () => {
         'polycam/poly.obj',
         function (object) {
           // load the texture
-          // model = { ...object };
+          model = { ...object };
+          modelContext.saveModel(model);
           const textureLoader = new THREE.TextureLoader();
-          const texture = textureLoader.load('polycam/texture.jpg');
+          const texture = textureLoader.load('polycam/texture-final-project.jpg');
 
           // append the texture to the model
           object.traverse(function (child) {
@@ -71,44 +79,96 @@ export const Model: React.FC = () => {
             }
           });
           scene.add(object);
-        },
-        function (xhr) {
-          console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        function (error) {
-          console.log('An error happened', error);
         }
+        // function (xhr) {
+        //   console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        // },
+        // function (error) {
+        //   console.log('An error happened', error);
+        // }
       );
     });
   };
 
-  // const calculateSphereRadiusOnEmbroidery = (embroideryCentre: number[], embroideryEnd: number[]) => {
-  //   const x = (embroideryEnd[0] - embroideryCentre[0]) * (embroideryEnd[0] - embroideryCentre[0]);
-  //   const y = (embroideryEnd[1] - embroideryCentre[1]) * (embroideryEnd[1] - embroideryCentre[1]);
-  //   const z = (embroideryEnd[2] - embroideryCentre[2]) * (embroideryEnd[2] - embroideryCentre[2]);
+  const calculateDistanceBetweenTwoPlanarPoints = (embroideryCentre: number[], embroideryEnd: number[]) => {
+    const x = (embroideryEnd[0] - embroideryCentre[0]) * (embroideryEnd[0] - embroideryCentre[0]);
+    const y = (embroideryEnd[1] - embroideryCentre[1]) * (embroideryEnd[1] - embroideryCentre[1]);
+    const z = (embroideryEnd[2] - embroideryCentre[2]) * (embroideryEnd[2] - embroideryCentre[2]);
 
-  //   const radius = Math.sqrt(x + y + z);
-  //   return radius;
-  // };
+    const radius = Math.sqrt(x + y + z);
+    return radius;
+  };
 
-  // const createSphere = (scene: THREE.Scene, embroideryCentre: number[], embroideryEnd: number[]) => {
-  //   const embroideryRadius = calculateSphereRadiusOnEmbroidery(embroideryCentre, embroideryEnd);
+  const createSphere = (scene: THREE.Scene, embroideryCentre: number[], embroideryEnd: number[]) => {
+    const embroideryRadius = calculateDistanceBetweenTwoPlanarPoints(embroideryCentre, embroideryEnd);
 
-  //   const geometry = new THREE.SphereGeometry( embroideryRadius, 32, 16 );
-  //   const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-  //   const sphere = new THREE.Mesh( geometry, material );
-  //   sphere.position.x = embroideryCentre[0];
-  //   sphere.position.y = embroideryCentre[1];
-  //   sphere.position.z = embroideryCentre[2];
+    const geometry = new THREE.SphereGeometry( embroideryRadius, 32, 16 );
+    const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+    const sphere = new THREE.Mesh( geometry, material );
+    sphere.position.x = embroideryCentre[0];
+    sphere.position.y = embroideryCentre[1];
+    sphere.position.z = embroideryCentre[2];
 
-  //   sphere.material.transparent = true;
-  //   sphere.material.opacity = 0.0;
+    sphere.material.transparent = true;
+    sphere.material.opacity = 0.0;
 
-  //   scene.add(sphere);
-  //   return sphere;
-  // };
+    scene.add(sphere);
+    return sphere;
+  };
 
-  // let sphere: THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial> | THREE.Object3D<THREE.Event>;
+  const closePopup = () => {
+    setShouldShowPopup(false);
+  };
+
+  // render the popup that will show when the user clicks on an embroidery
+  const renderPopup = (embroideryInfo: PopupEmbroideryInfo
+    ) => {
+    return (
+        <Popup
+          handleOnClickButton = {closePopup}
+          embroideryInfo = {embroideryInfo}
+        />        
+    )
+  };
+
+
+  // compute which sphere the user clicked on when they clicked on the model
+  const computeClickedOnSphere = (rayCaster: THREE.Raycaster, allSpheres: any, model: any) => {
+
+    const intersects = rayCaster.intersectObjects(model.children, true);
+    // if(intersects.length > 0) {
+    //   console.log('intersects', intersects[0].point);
+    // }
+
+    const raycastedSpheres: SphereInfo[] = [];
+    let raycastedSpheresLength: number;
+    let minimumDistanceToRay = 1000;
+    const closestSphere: SphereInfo = { embroideryId: "", sphere: new THREE.Object3D};
+    for (let i = 0; i < allSpheres.length; i++) {
+      const sphereIntersects = rayCaster.intersectObject(allSpheres[i].sphere);
+        if (sphereIntersects.length > 0) {
+          raycastedSpheresLength = raycastedSpheres.push(allSpheres[i]);
+        }
+    }
+
+    for (let i = 0; i < raycastedSpheres.length; i++) {
+      const embroideryId = raycastedSpheres[i].embroideryId;
+      const embroidery = AllEmbroideries.find(embroidery => embroidery.id == embroideryId) as Embroidery;
+
+      if (intersects.length > 0) {
+        const distance = calculateDistanceBetweenTwoPlanarPoints(intersects[0].point.toArray(), embroidery.centreCoordinates);
+
+        if (distance < minimumDistanceToRay) {
+          minimumDistanceToRay = distance;
+
+          closestSphere.sphere = raycastedSpheres[i].sphere;
+          closestSphere.embroideryId = raycastedSpheres[i].embroideryId;
+        }
+      }
+    }
+    return closestSphere;
+  };
+
 
   useEffect(() => {
     const canvas = document.getElementById('modelCanvas') as HTMLElement;
@@ -117,64 +177,87 @@ export const Model: React.FC = () => {
     renderer.setClearColor(0x000000, 0);
 
     // add controls to the able to move and zoom the model
-    const controls = new OrbitControls(camera, renderer.domElement);
+    // const controls = new OrbitControls(camera, renderer.domElement);
 
     // add lights to the model
     addModelLights();
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
 
     loadModel();
-    // sphere = createSphere(scene, embroideryCentre, embroideryEnd);
-    // sphereInfo = { embroideryId: "98ef96da-e1d3-49b0-8029-6bc405997dd7",
-    // sphere: sphere}
+
+    // create the spheres for the embroideries
+    for (let i = 0; i < AllEmbroideries.length; i++) {
+      // create the spheres
+      const embroidery = AllEmbroideries[i];
+      if (embroidery.centreCoordinates.length > 0) {
+        const sphere = createSphere(scene, embroidery.centreCoordinates, embroidery.endPointCoordinates);
+        spheresLength = spheres.push({ embroideryId: embroidery.id, sphere: sphere });
+      }
+    }
+
+    // save the generated spheres, camera, scene and renderer as global variables
+    modelContext.saveSpheres(spheres);
+    modelContext.saveCamera(camera);
+    modelContext.saveScene(scene);
+    modelContext.saveRenderer(renderer);
+  }, []);
+
+  useEffect(() => {
+    const controls = new OrbitControls(modelContext.camera, modelContext.renderer.domElement);
 
     // show the model
     function animate() {
       requestAnimationFrame(animate);
       controls.update();
-      renderer.render(scene, camera);
+      modelContext.renderer.render(modelContext.scene, modelContext.camera);
     }
     animate();
   });
 
-  // const handleClickOnCanvas = (event: any) => {
-  //   event.preventDefault();
+  const handleClickOnCanvas = (event: any) => {
+    event.preventDefault();
 
-  //   const canvas = document.getElementById('modelCanvas') as HTMLElement;
-  //   const { canvasPositionLeft,
-  //     canvasPositionTop,
-  //     canvasPositionHeight,
-  //     canvasPositionWidth } = getCanvasPosition(canvas);
+    const canvas = document.getElementById('modelCanvas') as HTMLElement;
+    const { canvasPositionLeft,
+      canvasPositionTop,
+      canvasPositionHeight,
+      canvasPositionWidth } = getCanvasPosition(canvas);
 
-  //   const x = ((event.clientX - canvasPositionLeft) / canvasPositionWidth) * 2 - 1;
-  //   const y = -((event.clientY - canvasPositionTop) / canvasPositionHeight) * 2 + 1;
+    const x = ((event.clientX - canvasPositionLeft) / canvasPositionWidth) * 2 - 1;
+    const y = -((event.clientY - canvasPositionTop) / canvasPositionHeight) * 2 + 1;
 
-  //   const mousePosition = new THREE.Vector2(x, y);
+    const mousePosition = new THREE.Vector2(x, y);
+    // console.log('camera', camera);
+    rayCaster.setFromCamera(mousePosition, modelContext.camera);
 
-  //   rayCaster.setFromCamera(mousePosition, camera);
-  //   const intersects = rayCaster.intersectObjects(model.children, true);
+    // scene.add(new THREE.ArrowHelper(rayCaster.ray.direction, rayCaster.ray.origin, 300, 0xff0000));
+    // const intersects = rayCaster.intersectObjects(modelContext.model.children, true);
 
-  //   const sphereIntersects = rayCaster.intersectObject(sphere);
+    // const sphereIntersects = rayCaster.intersectObject(sphere);
 
-  //   if (intersects.length > 0) {
-  //     console.log(intersects[0].point);
-  //     // scene.add(new THREE.ArrowHelper(rayCaster.ray.direction, rayCaster.ray.origin, 300, 0xff0000) );
-  //   }
+    // if (intersects.length > 0) {
+    //   console.log(intersects[0].point);
+    //   const scene = modelContext.scene;
+    //   scene.add(new THREE.ArrowHelper(rayCaster.ray.direction, rayCaster.ray.origin, 300, 0xff0000));
+    // }
 
-  //   if (sphereIntersects.length > 0) {
-  //     console.log('ayeeee');
-  //   }
+    // compute the first sphere that the ray goes through
+    const closestSphere = computeClickedOnSphere(rayCaster, modelContext.spheres, modelContext.model);
+    const sphereEmbroidery = AllEmbroideries.find(embroidery => embroidery.id == closestSphere.embroideryId);
 
-  //   // console.log('AllEmbroideries', AllEmbroideries.length);
-  //   // const embroidery = AllEmbroideries.find(embroidery => embroidery.id == "98ef96da-e1d3-49b0-8029-6bc405997dd7");
-  //   // console.log("embroidery.name", embroidery?.anatomyName);
-  // };
+    if (sphereEmbroidery) {
+      setPopupEmbroideryInfo({authorName: sphereEmbroidery?.authorName, anatomyName: sphereEmbroidery?.anatomyName, embroideryFileName: "thumbnail/" + sphereEmbroidery?.fileName});
+      setShouldShowPopup(true);
+    }
+  };
 
   return (
     <s.PageWrapper>
-      <s.ModelWrapper>
-        <s.ModelCanvas id="modelCanvas"></s.ModelCanvas>
-      </s.ModelWrapper>
+      {/* <s.ModelWrapper> */}
+      <s.ModelCanvas id="modelCanvas" onClick={handleClickOnCanvas}></s.ModelCanvas>
+      {/* </s.ModelWrapper> */}
+      {shouldShowPopup && renderPopup(popupEmbroideryInfo as PopupEmbroideryInfo)}
     </s.PageWrapper>)
 }
---
+++ frontend/src/components/Model/Popup.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable */
+import React from 'react';
+import * as s from './Popup.theme';
+import cat from './cat.jpg';
+import { PopupEmbroideryInfo } from '../../types/types';
+
+interface Props {
+  handleOnClickButton: () => void,
+  embroideryInfo: PopupEmbroideryInfo
+}
+
+export const Popup: React.FC<Props> = ({ handleOnClickButton, embroideryInfo }) => {
+  console.log(embroideryInfo.embroideryFileName);
+
+  const fileName = embroideryInfo.embroideryFileName as string;
+  const fileNamee = "favicon-16x16.png";
+  return (
+    <s.PopupContainer>
+      <s.ImageContainer>
+        {fileName && <s.EmbroideryImage src={fileName} alt="Embroidery Image" />}
+      </s.ImageContainer>
+      <s.AuthorName>Author Name: {embroideryInfo.authorName}</s.AuthorName>
+      <s.AnatomyName>Embroidery Name: {embroideryInfo.anatomyName}</s.AnatomyName>
+      <s.BackToModelButtonContainer>
+        <s.BackToModelButton onClick={handleOnClickButton}>Back to Model</s.BackToModelButton>
+      </s.BackToModelButtonContainer>
+    </s.PopupContainer>)
+}
