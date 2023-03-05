fdad5f9592958093190b6d9a9ab2d85fb2961604
Lucy Jefford <lucy.jefford@student.manchester.ac.uk>
2023-03-05 21:08:58 +0000

    Lucy's Blog -> Lucy's Website

--- frontend/src/components/toolbox/Footer/Footer.tsx
+++ frontend/src/components/toolbox/Footer/Footer.tsx
@@ -15,7 +15,7 @@ export const Footer: React.FC = () => {
                 {/* <a href="https://google.com" target="_blank">CSV</a> */}
                 <s.FooterLink href='https://www.museum.manchester.ac.uk/event/dab-hands-2/' target="_blank"> Resident Artist</s.FooterLink>
                 <p> | </p>
-                <s.FooterLink href='https://www.lucysart.co.uk/' target="_blank"> Lucy&apos;s Blog </s.FooterLink>
+                <s.FooterLink href='https://www.lucysart.co.uk/' target="_blank"> Lucy&apos;s Website </s.FooterLink>
                 <p> | </p>
                 <s.FooterLink href='https://www.donaldsammut.com/' target="_blank"> Donald Sammut </s.FooterLink>
                 <p> | </p>
@@ -24,7 +24,7 @@ export const Footer: React.FC = () => {
 
             <MediaQuery maxWidth={767}>
                 <s.MobileView>
-                    <s.FooterLink href='https://www.lucysart.co.uk/' target="_blank"> Lucy&apos;s Blog </s.FooterLink>
+                    <s.FooterLink href='https://www.lucysart.co.uk/' target="_blank"> Lucy&apos;s Website </s.FooterLink>
                     <p> | </p>
                     <s.Funders src={funders} mobile={true} loading='lazy' draggable='false' />
                 </s.MobileView>  
